8860a5ddb3e716079a72facccc935c4d957026b7
Math Costa <xcudder@hotmail.com>
2020-10-05 02:27:12 -0300

    Alter print_out_numbers argument on index.js

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ app.listen(3003, () => console.log("Listening on 3003"));
 const multiples = require('./js/multiples.js');
 
 app.get("/", (req, res)=>{
-    res.send(multiples.print_out_numbers(5))
+    res.send(multiples.print_out_numbers(100))
 })
